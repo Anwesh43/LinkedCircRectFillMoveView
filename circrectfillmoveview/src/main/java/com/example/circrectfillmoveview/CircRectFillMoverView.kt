bffa9263a9832827e104e5987647e696e174d1f6
Anwesh43 <anweshthecool0@gmail.com>
2020-12-30 12:41:22 +0530

    adding constants and fixes

--- circrectfillmoveview/src/main/java/com/example/circrectfillmoveview/CircRectFillMoverView.kt
+++ circrectfillmoveview/src/main/java/com/example/circrectfillmoveview/CircRectFillMoverView.kt
@@ -1,2 +1,27 @@
 package com.example.circrectfillmoveview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#4CAF50",
+    "#9C27B0",
+    "#795548",
+    "#2196F3"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val strokeFactor : Float = 90f
+val rectWFactor : Float = 3.9f
+val rectHFactor : Float = 8.9f
+val circleFator : Float = 6.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
